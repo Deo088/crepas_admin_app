c1dcd90b1949639643e65687220a1391d1ee39ac
Deo088 <diegoquintero.cal01@gmail.com>
2026-05-07 22:41:11 -0700

    feat:create product model

---
+++ lib/features/inventory/data/models/product_model.dart
@@ -0,0 +1,26 @@
+class ProductModel {
+  final int? id;
+  final String name;
+  final double price;
+  final String category;
+
+  ProductModel({
+    this.id,
+    required this.name,
+    required this.price,
+    required this.category,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'name': name, 'price': price, 'category': category};
+  }
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      id: map['id'],
+      name: map['name'],
+      price: map['price'],
+      category: map['category'],
+    );
+  }
+}
