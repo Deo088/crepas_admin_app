885b558430e70f8f3b13aaf882ebdee62affeb54
Deo088 <diegoquintero.cal01@gmail.com>
2026-05-09 08:13:04 -0700

    feat: implement product local datasource

---
+++ lib/features/inventory/data/datasources/product_local_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:sqflite/sqflite.dart';
+
+import 'package:crepas_admin_app/core/database/app_database.dart';
+import 'package:crepas_admin_app/features/inventory/data/models/product_model.dart';
+
+class ProductLocalDataSource {
+  Future<void> insertProduct(ProductModel product) async {
+    final Database db = await AppDatabase.database;
+
+    await db.insert('products', product.toMap());
+  }
+
+  Future<List<ProductModel>> getProducts() async {
+    final Database db = await AppDatabase.database;
+
+    final List<Map<String, dynamic>> maps = await db.query('products');
+
+    return List.generate(
+      maps.length,
+      (index) => ProductModel.fromMap(maps[index]),
+    );
+  }
+}
--- lib/features/inventory/presentation/inventory_screen.dart
+++ lib/features/inventory/presentation/inventory_screen.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
 
-class InventoryScreen extends StatelessWidget {
+import 'package:crepas_admin_app/features/inventory/data/datasources/product_local_datasource.dart';
+import 'package:crepas_admin_app/features/inventory/data/models/product_model.dart';
+
+class InventoryScreen extends StatefulWidget {
   const InventoryScreen({super.key});
 
   @override
+  State<InventoryScreen> createState() => _InventoryScreenState();
+}
+
+class _InventoryScreenState extends State<InventoryScreen> {
+  final ProductLocalDataSource dataSource = ProductLocalDataSource();
+
+  List<ProductModel> products = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadProducts();
+  }
+
+  Future<void> loadProducts() async {
+    final result = await dataSource.getProducts();
+
+    setState(() {
+      products = result;
+    });
+  }
+
+  Future<void> addProduct() async {
+    final product = ProductModel(
+      name: 'Crepa Oreo',
+      price: 85,
+      category: 'Crepas',
+    );
+
+    await dataSource.insertProduct(product);
+
+    loadProducts();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Inventory Screen')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Inventory')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addProduct,
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+
+          return ListTile(
+            title: Text(product.name),
+            subtitle: Text(product.category),
+            trailing: Text('\$${product.price}'),
+          );
+        },
+      ),
+    );
   }
 }
